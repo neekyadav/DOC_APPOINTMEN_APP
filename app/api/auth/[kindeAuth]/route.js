1ab8159382c5ecb62410dc25d6dd35d122ea92a7
NKY <neek@NKYs-MacBook-Air.local>
2025-04-18 20:53:48 +0530

    added

--- app/api/auth/[kindeAuth]/route.js
+++ app/api/auth/[kindeAuth]/route.js
@@ -1,11 +1,2 @@
-import { handleAuth } from '@kinde-oss/kinde-auth-nextjs/server';
-
-export default handleAuth({
-  authUrl: process.env.KINDE_ISSUER_URL,
-  clientId: process.env.KINDE_CLIENT_ID,
-  clientSecret: process.env.KINDE_CLIENT_SECRET,
-  siteUrl: process.env.NEXTAUTH_URL,
-  redirectUrl: process.env.KINDE_SITE_URL,
-  postLoginRedirectUri: process.env.KINDE_POST_LOGIN_REDIRECT_URL,
-  postLogoutRedirectUri: process.env.KINDE_POST_LOGOUT_REDIRECT_URL
-});
+import {handleAuth} from "@kinde-oss/kinde-auth-nextjs/server";
+export const GET = handleAuth();
