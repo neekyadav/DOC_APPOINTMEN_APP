f4fee57fcc6ef40bb2acc5bb803e21f15ec04982
NKY <neek@NKYs-MacBook-Air.local>
2025-04-19 13:04:55 +0530

    authroute updated

--- app/api/auth/[kindeAuth]/route.js
+++ app/api/auth/[kindeAuth]/route.js
@@ -1,8 +1,3 @@
-// import {handleAuth} from "@kinde-oss/kinde-auth-nextjs/server";
-// export const GET = handleAuth();
+import {handleAuth} from "@kinde-oss/kinde-auth-nextjs/server";
+export const GET = handleAuth();
 
-import { login } from '@kinde-oss/kinde-auth-nextjs/server';
-
-export async function GET(req) {
-  return login(req);
-}
