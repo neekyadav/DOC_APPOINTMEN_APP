98d93bf487b7a2cc3ff803d90fd1ab1071651efe
NKY <neek@NKYs-MacBook-Air.local>
2025-04-19 12:54:08 +0530

    kindeupdated

--- app/api/auth/[kindeAuth]/route.js
+++ app/api/auth/[kindeAuth]/route.js
@@ -1,2 +1,8 @@
-import {handleAuth} from "@kinde-oss/kinde-auth-nextjs/server";
-export const GET = handleAuth();
+// import {handleAuth} from "@kinde-oss/kinde-auth-nextjs/server";
+// export const GET = handleAuth();
+
+import { login } from '@kinde-oss/kinde-auth-nextjs/server';
+
+export async function GET(req) {
+  return login(req);
+}
